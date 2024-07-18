6df50c8d8c56c4da4f969f436d29b29806bad3de
Brighton Gold <brighton.m.gold@gmail.com>
2024-07-18 15:32:41 -0600

    try catch

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,57 +1,68 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
 
-
-const invCont = {}
+const invCont = {};
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
-}
+  try {
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+
+    const className = data[0].classification_name;
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 /* ***************************
  *  Build individual car details view
  * ************************** */
 invCont.buildCarById = async function (req, res, next) {
-  const inv_make = req.params.inv_make
-  const inv_model = req.params.inv_model
-  const inv_year = req.params.inv_year
-  const data = await invModel.getCarById(inv_make, inv_model, inv_year)
-  const detail = await utilities.buildCarDetail(data)
-  let nav = await utilities.getNav()
-  res.render("./inventory/car", {
-    title: inv_make + " " + inv_model,
-    nav,
-    car: data,
-    detail,
-  })
-}
+  try {
+    const inv_make = req.params.inv_make;
+    const inv_model = req.params.inv_model;
+    const inv_year = req.params.inv_year;
+    const data = await invModel.getCarById(inv_make, inv_model, inv_year);
+    const detail = await utilities.buildCarDetail(data);
+    let nav = await utilities.getNav();
+    res.render("./inventory/car", {
+      title: inv_make + " " + inv_model,
+      nav,
+      car: data,
+      detail,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 /* ***************************
  *  Render Inventory Management View
  * ************************** */
 invCont.renderManagementView = async function (req, res, next) {
-  let nav = await utilities.getNav()
-  let list = await utilities.buildManagementGrid()
-  res.render("./inventory/management", {
+  try {
+    let nav = await utilities.getNav();
+    let list = await utilities.buildManagementGrid();
+    res.render("./inventory/management", {
       title: "Manage Inventory",
       nav,
       list,
-      errors: null
-  })
-}
+      errors: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 /* ***************************
  *  Render Add New Inventory View
@@ -62,13 +73,13 @@ invCont.renderAddInventoryView = async function (req, res, next) {
     let classificationList = await utilities.buildClassificationList();
     let formData = req.body || {};
     let form = utilities.buildAddInventoryForm(formData, classificationList);
-    
+
     res.render("./inventory/add-inventory", {
       title: "Add New Inventory",
       nav,
       form,
       messages: req.flash("info"),
-      errors: req.flash("errors")
+      errors: req.flash("errors"),
     });
   } catch (error) {
     next(error);
@@ -79,16 +90,20 @@ invCont.renderAddInventoryView = async function (req, res, next) {
  *  Render Add New Classification View
  * ************************** */
 invCont.renderNewClassificationView = async function (req, res, next) {
-  let nav = await utilities.getNav();
-  let form = utilities.buildAddClassificationForm();
-  res.render("./inventory/add-classification", {
-    title: "Add New Classification",
-    nav,
-    form,
-    messages: req.flash("info"),
-    errors: req.flash("errors")
-  });
-}
+  try {
+    let nav = await utilities.getNav();
+    let form = utilities.buildAddClassificationForm();
+    res.render("./inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      form,
+      messages: req.flash("info"),
+      errors: req.flash("errors"),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 /* ***************************
  *  Add New Classification
@@ -96,18 +111,16 @@ invCont.renderNewClassificationView = async function (req, res, next) {
 invCont.addClassification = async function (req, res, next) {
   try {
     const { classification_name } = req.body;
-    console.log(req.body)
-    console.log(classification_name)
     const data = await invModel.addClassification(classification_name);
     if (data) {
       req.flash("info", "Classification added successfully");
     } else {
       req.flash("errors", "Failed to add classification");
     }
-   res.redirect("./inventory/add-classification");
+    res.redirect("./inventory/add-classification");
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = invCont
+module.exports = invCont;
